64682646a2d346d98e885b860d86b3d86a266f33
hfun <hfun2017@gmail.com>
2022-10-29 11:53:51 +0800

    加入了全局特征，和transformer的输出进行拼接。

--- models/trans_dgcnn.py
+++ models/trans_dgcnn.py
@@ -1,3 +1,5 @@
+import torch
+
 from models.dgcnn import Encoder
 from utils import *
 
@@ -7,18 +9,19 @@ class get_model(nn.Module):
         super(get_model, self).__init__()
         self.transformer = nn.Transformer(d_model, num_encoder_layers=3, num_decoder_layers=3)
         self.dgcnn=Encoder(d_model)
-        self.pointnetDecoder = DispGenerator(d_model)
+        self.pointnetDecoder = DispGenerator(d_model*2)
 
     def forward(self, encoder_input: torch.Tensor, decoder_input: torch.Tensor, **kwargs):
-        encoder_input, decoder_input = encoder_input.permute(0, 2, 1), decoder_input.permute(0, 2, 1)
-        embed_input = self.dgcnn(encoder_input)
-        embed_output = self.dgcnn(decoder_input)
+        encoder_input, decoder_input = encoder_input.permute(0, 2, 1), decoder_input.permute(0, 2, 1)       #[B,3,N]
+        embed_input = self.dgcnn(encoder_input)     # [B,D,N]
+        embed_output = self.dgcnn(decoder_input)       # [B,D,N]
+        global_feat = embed_output
         embed_input, embed_output = embed_input.permute(2, 0, 1), embed_output.permute(2, 0, 1)  # [N, B, d_model]
         transformer_out = self.transformer(embed_input, embed_output)  # [N, B ,d_model]
         transformer_out = transformer_out.permute(1, 0, 2)  # [B, N, d_model]
 
-        warpped_feat = transformer_out
-
+        warpped_feat = transformer_out      # [B,N,D]
+        warpped_feat = torch.cat([warpped_feat, global_feat.permute(0, 2, 1)], dim=-1) #[B,N,2D]
         displacement = self.pointnetDecoder(warpped_feat.permute(0, 2, 1))  # [B,3,N]
         warped = displacement + decoder_input[:, 0:3, :]
         loss1 = chamfer_loss(encoder_input[:, :3, :], warped, ps=warped.size()[-1])
